8324d8052461cbecc6eafa953ab2cc5bd53a94e5
ElieOaks <vde.joy@gmail.com>
2018-11-16 14:07:02 +0100

    stubbs of potential funktions needed, may be changes at will

--- security.py
+++ security.py
@@ -1,1 +1,11 @@
 #Securty module
+
+class security:
+
+    message = "some string"
+
+    def encrypt_message(message, key_one, key_two):
+        return 0
+    
+    def decrypt_message(cipher_text, key_One, key_two):
+        return 0
--- server.py
+++ server.py
@@ -1,1 +1,29 @@
 ##Server
+
+
+"""
+This is the server
+
+"""
+class Server:
+
+    list_of_clients = []
+    
+    
+
+    def give_me_IP(user_name):
+        return 0
+
+    def give_me_public_key(user_name):
+        return 0
+
+    def store_outgoing_messages(message, to_user, from_user):
+        return 0
+
+    def update_my_IP(user, new_IP):
+        return 0
+
+    def event_loop(incoming_event, from_user):
+        return 0
+
+
